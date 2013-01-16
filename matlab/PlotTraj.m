3d3606438367c834d22c0651ed9f52ebee575695
Justin Thomas <jut@seas.upenn.edu>
2013-01-16 18:18:53 -0500

    This could be somewhat handy.  Obviously, it still needs some work.

---
+++ matlab/PlotTraj.m
@@ -0,0 +1,20 @@
+function h = PlotTraj(traj)
+
+tstep = (traj.keytimes(end) - traj.keytimes(1))/100;
+tvec = traj.keytimes(1):tstep:traj.keytimes(end);
+
+val = TrajEval(traj, tvec);
+ndim = size(val, 2);
+
+h = zeros(ndim,1);
+for idx = 1:ndim
+    h(idx) = figure;
+    plot(tvec, val(:,idx,1));
+    title(['Dimension ', num2str(idx)])
+    xlabel('time (s)')
+    ylabel('position (m or rad)')
+    pause;
+    close
+end
+
+end
