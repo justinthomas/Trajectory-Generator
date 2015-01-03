03d31ba2a7e63e567bc1c15296a2877d28c01c1f
Giuseppe <loiannog@gmail.com>
2015-01-03 21:03:13 +0100

    fixing bug in nanwaypoints

--- matlab/NanWaypoint.m
+++ matlab/NanWaypoint.m
@@ -6,6 +6,6 @@ end
 
 temp = NaN(num_dimensions,1);
 
-output = struct('time',{time},'pos',{temp},'vel',{temp},'acc',{temp},'jerk',{temp});
+output = struct('time',{time},'pos',{temp},'vel',{temp},'acc',{temp},'jerk',{temp},'snap',{temp});
 
 end
